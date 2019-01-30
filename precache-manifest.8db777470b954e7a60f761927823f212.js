6b98642e581e934694eaecbacbdbb1e52d0a574d
uyoo <hs27951@gmail.com>
2019-01-30 22:38:48 +0900

    Updates

---
+++ precache-manifest.8db777470b954e7a60f761927823f212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c0206f086f073f03850",
+    "url": "/MovieApp_React/static/js/runtime~main.7c0206f0.js"
+  },
+  {
+    "revision": "7f51619d7e0936126cf1",
+    "url": "/MovieApp_React/static/js/main.7f51619d.chunk.js"
+  },
+  {
+    "revision": "7d735bbabbd9e1348679",
+    "url": "/MovieApp_React/static/js/1.7d735bba.chunk.js"
+  },
+  {
+    "revision": "7f51619d7e0936126cf1",
+    "url": "/MovieApp_React/static/css/main.f701d26d.chunk.css"
+  },
+  {
+    "revision": "893a08db8a876ddaf62830af052a0264",
+    "url": "/MovieApp_React/index.html"
+  }
+];
