0c4b4c31af5549309ddf7119d1ea980a36cb0026
uyoo <hs27951@gmail.com>
2019-01-30 15:15:03 +0900

    Component and Props function

---
+++ src/Movie.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+class Movie extends Component{
+
+    //부모 컴포넌트에게서 받는 정보의 타입을 체크
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string
+    };
+
+    render(){
+        return(
+            <div>
+                <MoviePoster title={this.props.title} poster={this.props.poster} />
+                <h3>{this.props.title}</h3>
+            </div>
+        );
+    }
+}
+
+class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
+    render(){
+        return(
+            <img src={this.props.poster} alt={this.props.title}></img>
+        );
+    }
+}
+
+export default Movie;
